e4af65bf625f9ebf698e5fe0858247efd0450ed9
dhanujadhav29 <59407746+dhanujadhav29@users.noreply.github.com>
2021-12-02 14:57:19 +0530

    MVC 1st Application

---
+++ EmployeeVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcDemoApplication.ViewModel
+{
+    public class EmployeeVM
+    {
+        public int eno{ set; get; }
+
+        public string ename { set; get; }
+
+        public decimal salary { set; get; }
+
+        public int dno { set; get; }    
+    }
+}
