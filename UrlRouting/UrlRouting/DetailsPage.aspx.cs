7c79731cb19285fc515d1b2cd743cc032154909d
dhanujadhav29 <dhanu.jadhav.2902@gmail.com>
2021-12-02 20:15:25 +0530

    URL Prgoram

---
+++ UrlRouting/UrlRouting/DetailsPage.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UrlRouting
+{
+    public partial class DetailsPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.RouteData.Values["Id"].ToString()!=null) { 
+
+            string rid=Page.RouteData.Values["Id"].ToString();
+            SqlConnection con = new SqlConnection("Server=.;Database=Assign1;trusted_connection=true");
+            SqlCommand cmd = new SqlCommand("Select * From Article where id="+rid, con);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            string title = ds.Tables[0].Rows[0]["Title"].ToString();
+            string desc = ds.Tables[0].Rows[0]["Desc"].ToString();
+                label1.Text = title;
+                label2.Text =desc;
+            Page.DataBind();
+            }
+
+        }
+    }
+}
